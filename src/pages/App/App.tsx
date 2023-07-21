780fcfa556ca0ef91cdc151c68ebdc4e3d114d7e
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-07-21 10:24:16 +0900

    :gift: feat: style normalize 세팅

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,5 +1,22 @@
+import normalize from "emotion-normalize";
+import { css, Global } from "@emotion/react";
+
 function App() {
-  return <>hello world!</>;
+  return (
+    <>
+      <Global
+        styles={css`
+          ${normalize}
+          h1, h2, h3, h4, h5, h6 {
+            font-size: 1em;
+            font-weight: normal;
+            margin: 0; /* or ‘0 0 1em’ if you’re so inclined */
+          }
+        `}
+      />
+      <p>hello world!</p>
+    </>
+  );
 }
 
 export default App;
